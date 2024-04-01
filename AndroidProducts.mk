f49568052093e64139a30d18c2d102d53e8072cb
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2024-04-01 11:15:23 -0300

    x1s: Adapt to PixelExperience

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_x1s.mk
+    $(LOCAL_DIR)/aosp_x1s.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_x1s-user \
-    lineage_x1s-userdebug \
-    lineage_x1s-eng
+    aosp_x1s-user \
+    aosp_x1s-userdebug \
+    aosp_x1s-eng
